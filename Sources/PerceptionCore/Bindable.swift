60714f689e5d44f1b49c0e0e1fcb85f749a84c4f
Miguel Cabeça <miguel.cabeca@gmail.com>
2025-03-18 09:39:15 +0100

    Even more availability annotations.

--- Sources/PerceptionCore/Bindable.swift
+++ Sources/PerceptionCore/Bindable.swift
@@ -23,6 +23,11 @@
 
     /// The bindable wrapper for the object that creates bindings to its properties using dynamic
     /// member lookup.
+    @available(iOS, introduced: 13, obsoleted: 17, message: "Use @Bindable without the 'Perception.' prefix.")
+    @available(macOS, introduced: 10.15, obsoleted: 14, message: "Use @Bindable without the 'Perception.' prefix.")
+    @available(tvOS, introduced: 13, obsoleted: 17, message: "Use @Bindable without the 'Perception.' prefix.")
+    @available(watchOS, introduced: 6, obsoleted: 10, message: "Use @Bindable without the 'Perception.' prefix.")
+    @available(visionOS, unavailable)
     public var projectedValue: Bindable<Value> {
       self
     }
@@ -49,6 +54,11 @@
     }
 
     /// Creates a bindable from the value of another bindable.
+    @available(iOS, introduced: 13, obsoleted: 17, message: "Use @Bindable without the 'Perception.' prefix.")
+    @available(macOS, introduced: 10.15, obsoleted: 14, message: "Use @Bindable without the 'Perception.' prefix.")
+    @available(tvOS, introduced: 13, obsoleted: 17, message: "Use @Bindable without the 'Perception.' prefix.")
+    @available(watchOS, introduced: 6, obsoleted: 10, message: "Use @Bindable without the 'Perception.' prefix.")
+    @available(visionOS, unavailable)
     public init(projectedValue: Bindable<Value>) where Value: AnyObject & Perceptible {
       self = projectedValue
     }
